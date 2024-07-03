550bbd3ecba66e6532cb2ca27e86893ce919d2d6
LuciaRuy <luciaruy1@gmail.com>
2024-07-03 15:46:15 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+  let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q"];
+  generateNewCard(values, palos);
 };
+
+function generateNewCard(numArr, paloArr) {
+  let num = Math.floor(Math.random() * numArr.length);
+  let palo = Math.floor(Math.random() * paloArr.length);
+
+  let paloArriba = document.querySelector("#topPalo");
+  let paloAbajo = document.querySelector("#bottomPalo");
+
+  paloArriba.innerHTML = paloArr[palo];
+
+  if (paloArr[palo] === "♥" || paloArr[palo] === "♦") {
+    paloArriba.style.color = "red";
+    paloAbajo.style.color = "red";
+  }
+
+  document.querySelector("#bottomPalo").innerHTML = paloArriba.innerHTML;
+  document.querySelector("#Value").innerHTML = numArr[num];
+}
